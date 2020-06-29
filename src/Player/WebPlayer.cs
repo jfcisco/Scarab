a91c7f7a68fd86f0b37f0db50b28f989045ba426
Jasper Francisco <jasper.francisco@gmail.com>
2020-06-29 09:00:34 +0800

    Refactor WebPlayer
    Changed to implement new interface methods. Viewing the episode has
    been separated from the Play method. A ToggleFullScreen method is also
    added to handle fullscreen.

--- src/Player/WebPlayer.cs
+++ src/Player/WebPlayer.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 using System;
 
 namespace Player
@@ -7,7 +9,8 @@ namespace Player
     public class WebPlayer : IPlayer, IDisposable
     {
         private const string STREAM_SOURCE = @"https://twist.moe/a/";
-        private const string CHROME_PATH = @"/Users/ramelb.francisco/Documents/Jasper/learn-dotnet/Scarab/bin/";
+        private readonly string CHROME_PATH = Environment.GetEnvironmentVariable("CHROMEDRIVER_PATH");
+        private readonly WebDriverWait wait;
         private IWebDriver driver;
         public WebPlayer()
         {
@@ -17,30 +20,57 @@ namespace Player
 
             // create webdriver
             driver = new ChromeDriver(CHROME_PATH, options);
-            driver.Manage().Window.Maximize();
+            driver.Manage().Window.Maximize(); 
+
+            // Configure explicit wait for UI interactions
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
         }
 
-        /// <summary>Gets a relative URL and plays the first episode of the anime
+        /// <summary>
+        /// Changes the anime being played to the one whose relative URL is passed
         /// </summary>
-        public void Play(string relativeUrl)
+        /// <param name="relativeUrl"></param>
+        /// <param name="episode"></param>
+        public void ViewAnime(string relativeUrl, int episode = 1)
         {
-            string fullUrl = STREAM_SOURCE + relativeUrl;
+            string fullUrl = STREAM_SOURCE + relativeUrl + "/" + episode;
             // Go to the relativeUrl
             driver.Navigate().GoToUrl(fullUrl);
+            // ToggleFullScreen();
         }
 
-        /// <summary>Play an anime's specific episode
+        /// <summary>
+        /// Presses the Play button
         /// </summary>
-        public void Play(string relativeUrl, int episodeNumber)
+        public void Play()
         {
-            string fullUrl = STREAM_SOURCE + relativeUrl + "/" + episodeNumber.ToString();
-            // Go to the relativeUrl
-            driver.Navigate().GoToUrl(fullUrl);
+            // Click Video to toggle play status
+            IWebElement videoElement = driver.FindElement(By.CssSelector("video"));
+            videoElement.Click();
+        }
+
+        /// <summary>
+        /// Toggle fullscreen status of player
+        /// </summary>
+        public void ToggleFullScreen()
+        {
+            // Double Click Video to Toggle Fullscreen
+            IWebElement videoElement = driver.FindElement(By.CssSelector("video"));
+            new Actions(driver).DoubleClick(videoElement).Perform();
+        }
+
+        /// <summary>
+        ///  Pause the WebPlayer
+        /// </summary>
+        public void Pause()
+        {
+            driver.FindElement(By.CssSelector(".play-pause-btn")).Click();
         }
 
         // Quit the driver to prevent problems
         public void Dispose()
         {
+            driver.Close();
             driver.Quit();
         }
     }
